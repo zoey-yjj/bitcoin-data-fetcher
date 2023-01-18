e1b2417b549f39d9a74d4f37480451dde2c27b26
zoey-yjj <zoeyyyjj@gmail.com>
2023-01-18 14:15:02 +0800

    add comments

--- main.cpp
+++ main.cpp
@@ -3,15 +3,22 @@
 #include <iostream>
 
 int main() {
+    // Crypto data to be collected
     std::string apiKey = "free-api-get-from-coin-market"; // Replace with your actual API key
     std::string symbol = "BTC";
     std::string base = "USD";
 
+    // initialise CoinMarketCap with apiKey
     CoinMarketCap coinMarketCap(apiKey);
+
+    // get data in jason format using getMarketData function
     Json::Value coinData = coinMarketCap.getMarketData(symbol, base);
 
+    // if data size is zero, there is no related information for required pair Symbol/Base
     if (coinData.size() == 0) {
         std::cout << "Symbol/Base: " << symbol << "/" << base << " is not available..." << std::endl;
+    
+    // data is received, print out for checking
     } else {
         std::cout << "Symbol/Base: " << symbol << "/" << base << std::endl;
         std::cout << "Price: " << coinData["price"].asFloat() << std::endl;
